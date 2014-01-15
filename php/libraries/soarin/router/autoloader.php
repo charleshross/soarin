720a7d8d46c83a4ffd495688cd40cfbfdda77f92
Charles Ross <charleshross@gmail.com>
2014-01-14 19:58:10 -0800

    fixed

--- php/libraries/soarin/router/autoloader.php
+++ php/libraries/soarin/router/autoloader.php
@@ -22,14 +22,14 @@ function soarin_autoloader($input_classname) {
 	}
 	
 	// Application
-	$project_controllers = APP . '/' . str_replace('_', '/', $input_classname) . '.php';
-	$project_controllers_c = APP . '/' . str_replace('_', '/', lcfirst($input_classname)) . '.php';
+	$project_app_class = APP . '/' . str_replace('_', '/', $input_classname) . '.php';
+	$project_app_class_c = APP . '/' . str_replace('_', '/', lcfirst($input_classname)) . '.php';
 	
-	if (file_exists($project_controllers)) {
-		require_once ($project_controllers);
+	if (file_exists($project_app_class)) {
+		require_once ($project_app_class);
 		return;
-	} else if (file_exists($project_controllers_c)) {
-		require_once ($project_controllers_c);
+	} else if (file_exists($project_app_class_c)) {
+		require_once ($project_app_class_c);
 		return;
 	}
 	
